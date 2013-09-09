19c177a2247ade96e2ad85be8520036e9b125376
Walter Bright <walter@walterbright.com>
2013-09-08 17:08:13 -0700

    add scripts

---
+++ all2.sh
@@ -0,0 +1,79 @@
+FROMDIR=/home/walter/newsgroups/articles
+TODIR=/home/walter/newsgroups/todir
+SITEDIR=/usr/local/www/digitalmars.com/data/d/archives
+FOO=./foo2
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) D/gnu
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) D
+
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) digitalmars/D
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) digitalmars/D/bugs
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) digitalmars/D/dtl
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) digitalmars/D/ide
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) digitalmars/D/ldc
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) digitalmars/D/announce
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) digitalmars/D/learn
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) digitalmars/D/dwt
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) digitalmars/D/debugger
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) digitalmars/empire
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) DMDScript
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) electronics
+
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) c++
+
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) c++/announce
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) c++/atl
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) c++/beta
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) c++/chat
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) c++/command-line
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) c++/dos
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) c++/dos/16-bits
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) c++/dos/32-bits
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) c++/idde
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) c++/mfc
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) c++/rtl
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) c++/stl
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) c++/stl/hp
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) c++/stl/port
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) c++/stl/sgi
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) c++/stlsoft
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) c++/windows
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) c++/windows/16-bits
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) c++/windows/32-bits
+
+$(FOO) $(FROMDIR) $(TODIR) $(SITEDIR) c++/wxwindows
+
+
